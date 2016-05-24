c696180eb66348c566ee91035fa2780c8a5b8c85
Idan Kamara <idankk86@gmail.com>
2016-05-24 15:30:30 -0700

    config: make it possible to setup logging

--- showdocs/__init__.py
+++ showdocs/__init__.py
@@ -10,3 +10,27 @@ if config.TEST:
     from showdocs import debugviews
 
 app.config.from_object(config)
+
+_addedhandlers = False
+def setuplogging():
+    import logging, sys
+    root = logging.getLogger('showdocs')
+    root.setLevel(logging.INFO)
+
+    if not config.LOG:
+        root.setLevel(logging.CRITICAL)
+        return
+
+    global _addedhandlers
+    if _addedhandlers:
+        return
+    _addedhandlers = True
+
+    formatter = logging.Formatter('%(asctime)s %(name)s[%(levelname)s] %(message)s')
+    sh = logging.StreamHandler(sys.stdout)
+    sh.setLevel(logging.INFO)
+    sh.setFormatter(formatter)
+    root.setLevel(logging.INFO)
+    root.addHandler(sh)
+
+setuplogging()
--- showdocs/config.py
+++ showdocs/config.py
@@ -6,4 +6,14 @@ def _np(p):
 ROOT = _np(os.path.join(os.path.dirname(__file__), '..'))
 STATIC_DIR = _np(os.path.join(ROOT, 'showdocs', 'static'))
 
+STAGING_DIR = _np(os.path.join(ROOT, 'staging'))
+EXTERNAL_DIR = _np(os.path.join(ROOT, 'external'))
+
 TEST = True
+LOG = False
+
+def update(d):
+    globals().update(d)
+
+    from showdocs import setuplogging
+    setuplogging()
